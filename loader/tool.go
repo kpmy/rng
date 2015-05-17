a63c8f62544a02bd6a97c337229a166f41b2cb5f
kpmy <petryxa.clever@gmail.com>
2015-05-17 22:13:33 +0300

    реализовал загрузку схем и приступил к разбору

--- loader/tool.go
+++ loader/tool.go
@@ -1,11 +1,48 @@
 package loader
 
 import (
+	"github.com/kpmy/ypk/halt"
 	"io"
 )
+
+type Walker struct {
+	root *Node
+}
+
+func (w *Walker) forEach(n *Node, do func(w *Walker, n *Node)) {
+	for _, v := range n.Inner {
+		do(w, &v)
+	}
+}
+
+func traverseWrap() func(w *Walker, n *Node) {
+	return func(w *Walker, n *Node) {
+		w.traverse(n)
+	}
+}
+
+func (w *Walker) traverse(n *Node) {
+	switch n.XMLName.Local {
+	case "grammar":
+		if start := n.FindByXMLName("start"); start != nil {
+			w.forEach(start, traverseWrap())
+		}
+	case "ref":
+		if ref := w.root.FindByName(n.Name); ref != nil {
+			w.forEach(ref, traverseWrap())
+		} else {
+			halt.As(100, "ref not found", n.Name)
+		}
+	default:
+		halt.As(100, n.XMLName)
+	}
+}
 
 func Load(input io.Reader) {
 	//only xml rng format for now
 	xrd := &XMLReader{rd: input}
-	xrd.Read()
+	if data, err := xrd.Read(); err == nil {
+		w := &Walker{root: data}
+		w.traverse(data)
+	}
 }
--- loader/xml.go
+++ loader/xml.go
@@ -5,36 +5,75 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io"
+	"strings"
 )
 
 type XMLReader struct {
 	rd io.Reader
 }
 
-type Pattern struct {
+type Node struct {
+	XMLName  xml.Name
+	Name     string `xml:"name,attr"`
+	NS       string `xml:"ns,attr"`
+	Type     string `xml:"type,attr"`
+	Combine  string `xml:"combine,attr"`
+	DataType string `xml:"datatypeLibrary,attr"`
+	CharData string `xml:",chardata"`
+	Inner    []Node `xml:",any"`
 }
 
-func (p *Pattern) String() string {
-	return "pattern"
+func (p *Node) Data() string {
+	return strings.Trim(p.CharData, " \n\r\t")
 }
 
-type Grammar struct {
-	PatternList []Pattern `xml:"define"`
-	Start       []Pattern `xml:"start"`
-}
-
-func (g *Grammar) String() (ret string) {
-	ret = fmt.Sprintln("grammar defines ", len(g.PatternList), " patterns")
-	for _, v := range g.PatternList {
-		ret = fmt.Sprintln(ret, &v)
+func (p *Node) String() (ret string) {
+	ret = fmt.Sprint("$", p.XMLName.Local, " ")
+	if p.Name != "" {
+		ret = fmt.Sprint(ret, "name: ", p.Name, " ")
+	}
+	if p.NS != "" {
+		ret = fmt.Sprint(ret, "ns: ", p.NS, " ")
+	}
+	if p.Type != "" {
+		ret = fmt.Sprint(ret, "type: ", p.Type, " ")
+	}
+	if p.Data() != "" {
+		ret = fmt.Sprint(ret, "data: ", p.Data(), " ")
+	}
+	if len(p.Inner) > 0 {
+		ret = fmt.Sprintln(ret)
+	}
+	for _, v := range p.Inner {
+		ret = fmt.Sprint(ret, "\n", &v)
 	}
 	return
 }
 
-func (x *XMLReader) Read() {
-	root := &Grammar{}
+func (n *Node) FindByXMLName(name string) (ret *Node) {
+	for i := 0; i < len(n.Inner) && ret == nil; i++ {
+		if n.Inner[i].XMLName.Local == name {
+			ret = &n.Inner[i]
+		}
+	}
+	return
+}
+
+func (n *Node) FindByName(name string) (ret *Node) {
+	for i := 0; i < len(n.Inner) && ret == nil; i++ {
+		if n.Inner[i].Name == name {
+			ret = &n.Inner[i]
+		}
+	}
+	return
+}
+
+func (x *XMLReader) Read() (root *Node, err error) {
+	root = &Node{}
 	buf := bytes.NewBuffer(nil)
-	io.Copy(buf, x.rd)
-	xml.Unmarshal(buf.Bytes(), root)
-	fmt.Println(root)
+	if _, err = io.Copy(buf, x.rd); err == nil {
+		err = xml.NewDecoder(buf).Decode(root)
+		//fmt.Println(root)
+	}
+	return
 }
